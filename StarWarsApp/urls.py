c1021e21cde9aa912a941a327151120acd9cd782
Kasempiternal <36620658+Kasempiternal@users.noreply.github.com>
2021-12-14 12:48:26 +0100

    Fotos Funsionando

--- StarWarsApp/urls.py
+++ StarWarsApp/urls.py
@@ -4,9 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.home,name='home'),
-   path('buscarPersonajes/<int:id_raza>',views.buscarPersonajesRaza, name = 'buscarPersonajesRaza'),
-   path('buscarPersonajes/<int:id_raza>/<int:id_pelicula>', views.buscarPersonajesRazaYPelicula, name = 'buscarPersonajesRazaYPelicula'),
+   path('',views.index,name='home'),
+   path('buscarPersonajes',views.buscarPersonajes, name = 'buscarPersonajes'),
+   path('buscarPersonajes/<raza_id>',views.buscarPersonajes2, name='buscarPersonajes2'),
    path('test1',views.test1,name='test1'),
    path('test2',views.test2,name='test2')
 

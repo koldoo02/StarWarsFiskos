a4c77ea3d411ef95b567413a46448c6db87da112
Kasempiternal <36620658+Kasempiternal@users.noreply.github.com>
2021-12-17 13:56:13 +0100

    Aleluya el boton funsio

--- StarWarsApp/urls.py
+++ StarWarsApp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.index,name='home'),
+   path('home',views.index,name='home'),
    path('buscarPersonajes',views.buscarPersonajes, name = 'buscarPersonajes'),
    path('test1',views.test1,name='test1'),
    path('test2',views.test2,name='test2')
--- StarWarsApp/views.py
+++ StarWarsApp/views.py
@@ -7,23 +7,32 @@ def index(request):
     peliculas= Pelicula.objects.order_by('id').all()
     razas= Raza.objects.order_by('id').all()
     personajes=Personaje.objects.all()
+
     return render(request,"index.html",{'lista_peliculas' : peliculas,'lista_razas' : razas,'lista_personajes' : personajes})
 
+def buscarPersonajes(request):
+    results=request.GET['razaseleccionada']
+    results2=request.GET['peliculaseleccionada'] 
 
+    peliculas= Pelicula.objects.order_by('id').all()
+    razas= Raza.objects.order_by('id').all()
+    personajes=Personaje.objects.all()
+
+    return render(request,"buscarPersonajes.html",{'razaseleccionada':results,'peliculaseleccionada':results2,'lista_peliculas' : peliculas,'lista_razas' : razas,'lista_personajes' : personajes})
+
+
+
+    
 def test1(request):
-    peliculas= Pelicula.objects.order_by('id')
-    razas= Raza.objects.order_by('id')
-    personajes= Personaje.objects.all()
+    peliculas= Pelicula.objects.order_by('id').all()
+    razas= Raza.objects.order_by('id').all()
+    personajes=Personaje.objects.all()
+
     return render(request,"test1.html",{'lista_peliculas' : peliculas,'lista_razas' : razas,'lista_personajes' : personajes})
 
 def test2(request):
     results=request.GET['razaseleccionada']
-    results2=request.GET['peliculaseleccionada']
+    results2=request.GET['peliculaseleccionada'] 
     return render(request,"test2.html",{'razaseleccionada':results,'peliculaseleccionada':results2})
 
-    
-def buscarPersonajes(request):
-    results=request.GET['razaseleccionada']
-    results2=request.GET['peliculaseleccionada'] 
-    return render(request,{'razaseleccionada':results,'peliculaseleccionada':results2})
 
